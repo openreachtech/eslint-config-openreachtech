1c5faa77c15eb6311e34789299e8222ed5c6172c
Stew Eucen <stew.eucen@gmail.com>
2023-10-27 22:28:30 +0900

    Add exempted file for jsdoc/multiline-blocks

---
+++ tests/exempted/jsdoc/multiline-blocks.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {string} */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
+const alpha = 'alpha'
+
+/** @lends AlphaLendsClass */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
+
+/**
+ * Alpha class for @lends.
+ */
+class AlphaLendsClass extends Object {
+  /** @inheritdoc */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
+  toString () {
+    return this.toString()
+  }
+
+  /** @override */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
+  valueOf () {
+    return this.valueOf()
+  }
+}
+
+module.exports = {
+  alpha,
+}
