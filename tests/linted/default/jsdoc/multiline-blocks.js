eacb05ae8c2d002ed8325ef3e5477045621b311e
Stew Eucen <stew.eucen@gmail.com>
2023-10-27 22:28:49 +0900

    Update linted file for jsdoc/multiline-blocks

--- tests/linted/default/jsdoc/multiline-blocks.js
+++ tests/linted/default/jsdoc/multiline-blocks.js
@@ -5,18 +5,23 @@
  * because the rest is multiline
  */
 
-/** @type {string} */ // ✅ singleLineTags:[lends, type, inheritdoc] of `jsdoc/multiline-blocks`
+/** @type {string} */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
 const alpha = 'alpha'
 
-/** @lends AlphaLendsClass */ // ✅ singleLineTags:[lends, type, inheritdoc] of `jsdoc/multiline-blocks`
+/** @lends AlphaLendsClass */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
 
 /**
  * Alpha class for @lends.
  */
 class AlphaLendsClass extends Object {
-  /** @inheritdoc */ // ✅ singleLineTags:[lends, type, inheritdoc] of `jsdoc/multiline-blocks`
+  /** @inheritdoc */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
   toString () {
     return this.toString()
+  }
+
+  /** @override */ // ✅ singleLineTags:[lends, type, inheritdoc, override] of `jsdoc/multiline-blocks`
+  valueOf () {
+    return this.valueOf()
   }
 }
 
