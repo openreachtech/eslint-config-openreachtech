b02ae1fe677a4cff8bfc8190c35920653113df5b
Stew Eucen <stew.eucen@gmail.com>
2023-10-27 21:59:28 +0900

    Add litend file of id-length

---
+++ tests/linted/standard/id-length.js
@@ -0,0 +1,52 @@
+'use strict'
+
+/**
+ * Lint sample for id-length rule.
+ */
+class IdLength {
+  /**
+   * A method.
+   *
+   * @param {number} first - A number.
+   * @returns {number} - Single A.
+   */
+  a (first) { // ❌ `id-length`
+    return first + first
+  }
+
+  /**
+   * X method.
+   *
+   * @param {number} x - A number.
+   * @returns {number} - Single A.
+   */
+  power (x) { // ❌ `id-length`
+    return x * x
+  }
+
+  /**
+   * Getter: handling.
+   *
+   * @returns {object} - An object.
+   */
+  get $ () { // ✅ `id-length`
+    return {}
+  }
+
+  /**
+   * Alpha method.
+   *
+   * @param {Array} array - Array of number.
+   * @returns {object} - An object.
+   */
+  alphaMethod (array) {
+    return array.map(
+      (
+        _, // ✅ `id-length`
+        index
+      ) => index
+    )
+  }
+}
+
+module.exports = IdLength
