fbc11e61f3d65a55b77abbe865e8940c7e572f86
Stew Eucen <stew.eucen@gmail.com>
2023-10-28 09:59:48 +0900

    Add exempted file for jsdoc/match-description

---
+++ tests/exempted/jsdoc/match-description.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/* eslint-disable jsdoc/require-jsdoc */
+
+class MatchDescription {
+  /* eslint-disable jsdoc/informative-docs */
+  // ✅ matchDescription of `match-description`
+  /**
+   * Constructor.
+   */
+  constructor () {
+    this.alpha = 1
+    this.beta = 'string'
+  }
+  /* eslint-enable jsdoc/informative-docs */
+
+  /* eslint-disable jsdoc/require-description-complete-sentence */
+  // ✅ matchDescription of `match-description` (does not start with capitalized letter)
+  /**
+   * get alpha value.
+   *
+   * @returns {number} - Alpha value.
+   */
+  firstMethod () {
+    return this.alpha
+  }
+  /* eslint-enable jsdoc/require-description-complete-sentence */
+
+  /* eslint-disable jsdoc/require-description-complete-sentence */
+  // ✅ matchDescription of `match-description` (no period)
+  /**
+   * Get beta value
+   *
+   * @returns {string} - Beta value.
+   */
+  secondMethod () {
+    return this.beta
+  }
+  /* eslint-enable jsdoc/require-description-complete-sentence */
+}
+
+module.exports = MatchDescription
