a136631eab3c686499753d0feee67004983c566c
Stew Eucen <stew.eucen@gmail.com>
2024-05-30 08:42:39 +0900

    Distribute openraechtech plugin configuration from eslint.config.js

---
+++ configurations/plugins/openreachtech.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const openreachtechPlugin = require('eslint-plugin-openreachtech')
+
+module.exports = {
+  plugins: {
+    openreachtech: openreachtechPlugin,
+  },
+
+  rules: {
+    'openreachtech/empty-line-after-super': [
+      'error',
+    ],
+    'openreachtech/indent-in-infix-expression': [
+      'error',
+    ],
+    'openreachtech/newline-per-parameter': [
+      'error',
+    ],
+    'openreachtech/no-if-in-oneline': [
+      'error',
+    ],
+    'openreachtech/no-unexpected-multiline': [
+      'error',
+    ],
+  },
+}
