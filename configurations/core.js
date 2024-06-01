f32c39f5ebed8bc0f3bcb979246178e15f3c5c25
Stew Eucen <stew.eucen@gmail.com>
2024-06-02 01:23:18 +0900

    Fulfill options of no-console in core.js

--- configurations/core.js
+++ configurations/core.js
@@ -7,11 +7,6 @@ module.exports = {
     ...configurationHash.core.rules,
     ...configurationHash.disableCoreStylistic.rules, // not required to add after v10
 
-    'no-console': [
-      'error',
-      // { allow: [] }
-    ],
-
     'no-param-reassign': [
       'error',
       {
@@ -184,6 +179,34 @@ module.exports = {
       {
         allow: [],
         int32Hint: false,
+      },
+    ],
+    'no-console': [
+      'error',
+      {
+        allow: undefined, // When disable `allow` field, give undefined instead of empty array
+        // [
+        //   'assert',
+        //   'clear',
+        //   'Console',
+        //   'count',
+        //   'countReset',
+        //   'debug',
+        //   'dir',
+        //   'dirxml',
+        //   'error',
+        //   'group',
+        //   'groupCollapsed',
+        //   'groupEnd',
+        //   'info',
+        //   'log',
+        //   'table',
+        //   'time',
+        //   'timeEnd',
+        //   'timeLog',
+        //   'trace',
+        //   'warn',
+        // ],
       },
     ],
     'no-continue': [
