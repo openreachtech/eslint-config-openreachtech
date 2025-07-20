8bc86ce989b4ce09d28d11fcc186ea6c1ced96df
Stew Eucen <stew.eucen@gmail.com>
2025-07-20 10:50:23 +0900

    Add linted test for for jest/max-nested-describe rule

---
+++ tests/linted/standard$/jest/max-nested-describe.js
@@ -0,0 +1,19 @@
+describe('Top Level Describe (1)', () => {
+  describe('Nested Describe (2)', () => {
+    describe('Nested Describe (3)', () => {
+      describe('Nested Describe (4)', () => {
+        describe('Nested Describe (5)', () => {
+          describe('Nested Describe (6)', () => { // ✅️ exempted { max: 8 } of `jest/max-nested-describe`
+            describe('Nested Describe (7)', () => { // ✅️ exempted { max: 8 } of `jest/max-nested-describe`
+              describe('Nested Describe (8)', () => { // ✅️ exempted { max: 8 } of `jest/max-nested-describe`
+                describe('Nested Describe (9)', () => { // ❌️ { max: 8 } of `jest/max-nested-describe`
+                  // noop
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+})
