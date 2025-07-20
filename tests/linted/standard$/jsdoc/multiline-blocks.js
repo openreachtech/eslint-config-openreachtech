c3848331b3b693963eff88e227ffddc0917744cc
Stew Eucen <stew.eucen@gmail.com>
2025-07-20 14:38:02 +0900

    Add linted test file for jsdoc/multiline-blocks ESLint rule

---
+++ tests/linted/standard$/jsdoc/multiline-blocks.js
@@ -0,0 +1,14 @@
+/** @param {string} value */ // ❌️ singleLineTags:[lends, type, extends, inheritdoc, override] of `jsdoc/multiline-blocks`
+function alphaFunction (value) {
+  // noop
+}
+
+/** @param {*} input */ // ❌️ singleLineTags:[lends, type, extends, inheritdoc, override] of `jsdoc/multiline-blocks`
+function betaFunction (input) {
+  // noop
+}
+
+export default {
+  alphaFunction,
+  betaFunction,
+}
