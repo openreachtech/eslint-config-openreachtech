2b0010ae8d2123fd2513207cee2042adddc4ba0e
Stew Eucen <stew.eucen@gmail.com>
2025-07-21 16:44:19 +0900

    Disable eslint-comments plugin rules for CI

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,20 @@
 import openreachtechEslintConfig from './lib/eslint.config.js'
 
-export default openreachtechEslintConfig
+export default [
+  ...openreachtechEslintConfig,
+
+  // Disable ESLint Comments rules for CI
+  {
+    rules: {
+      'eslint-comments/disable-enable-pair': 'off',
+      'eslint-comments/no-aggregating-enable': 'off',
+      'eslint-comments/no-duplicate-disable': 'off',
+      'eslint-comments/no-unlimited-disable': 'off',
+      'eslint-comments/no-unused-disable': 'off',
+      'eslint-comments/no-unused-enable': 'off',
+      'eslint-comments/no-restricted-disable': 'off',
+      'eslint-comments/no-use': 'off',
+      'eslint-comments/require-description': 'off',
+    },
+  },
+]
