8636cd6c3326921cacf8a68f317bb381959f4749
Stew Eucen <stew.eucen@gmail.com>
2025-07-28 15:22:39 +0900

    Update exempted test file for no-restricted-syntax for '~Data' suffix

--- tests/exempted/no-restricted-syntax.js
+++ tests/exempted/no-restricted-syntax.js
@@ -29,13 +29,21 @@ const isRadioNodeList = value => value instanceof RadioNodeList // ✅️ ignore
  */
 const transfer = new DataTransfer()
 
-transfer.getData() // ✅️ { selector: 'Identifier[name=/.+(?<!Form|get|set|clear)Data$/]' } of `no-restricted-syntax`
-transfer.setData('text/plain', 'value') // ✅️ { selector: 'Identifier[name=/.+(?<!Form|get|set|clear)Data$/]' } of `no-restricted-syntax`
-transfer.clearData() // ✅️ { selector: 'Identifier[name=/.+(?<!Form|get|set|clear)Data$/]' } of `no-restricted-syntax`
+transfer.getData() // ✅️ { selector: 'Identifier[name=/.+(?<!Form|get|set|clear|inline)Data$/]' } of `no-restricted-syntax`
+transfer.setData('text/plain', 'value') // ✅️ { selector: 'Identifier[name=/.+(?<!Form|get|set|clear|inline)Data$/]' } of `no-restricted-syntax`
+transfer.clearData() // ✅️ { selector: 'Identifier[name=/.+(?<!Form|get|set|clear|inline)Data$/]' } of `no-restricted-syntax`
+
+const gammaPayload = {
+  inlineData: { // ✅️ { selector: 'Identifier[name=/.+(?<!Form|get|set|clear|inline)Data$/]' } of `no-restricted-syntax`
+    mimeType: 'text/plain',
+    data: 'Hello World',
+  },
+}
 
 export default {
   alpha,
   betaValue,
   RequestInfo,
   isRadioNodeList,
+  gammaPayload,
 }
