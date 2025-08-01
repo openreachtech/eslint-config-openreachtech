452b8f733f77a9ce6bd2b1bb54d428b8d719bab7
Stew Eucen <stew.eucen@gmail.com>
2025-08-01 20:40:24 +0900

    Fix unnecessary lint error in linted test file of no-restricted-syntax for noIdentifierByCanceled

--- tests/linted/standard$/no-restricted-syntax/$noIdentifierByCanceled.js
+++ tests/linted/standard$/no-restricted-syntax/$noIdentifierByCanceled.js
@@ -1,7 +1,12 @@
 const cancelledAt = new Date() // ❌️ { selector: 'Identifier[name=/cancelled/i]' } of `no-restricted-syntax`
 
 class CancelledOrder { // ❌️ { selector: 'Identifier[name=/cancelled/i]' } of `no-restricted-syntax`
-  // noop
+  /**
+   * Constructor.
+   */
+  constructor () {
+    this.first = []
+  }
 }
 
 export default {
