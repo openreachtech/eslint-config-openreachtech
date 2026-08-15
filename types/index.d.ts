e67de7f3c89e3e5a6cbcadb588f75afe18b6b126
Stew Eucen <stew.eucen@gmail.com>
2026-08-16 00:36:06 +0900

    Add coreRuleOptionHash to types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,6 +10,34 @@ export const jsdocPluginConfig: Linter.FlatConfig
 export const openreachtechPluginConfig: Linter.FlatConfig
 export const stylisticPluginConfig: Linter.FlatConfig
 
+export const coreRuleOptionHash: {
+  'id-denylist': {
+    spreadOptions: Array<string>
+  }
+  'id-length': {
+    exceptions: Array<string>
+  }
+  'new-cap': {
+    capIsNewExceptions: Array<string>
+  }
+  'no-restricted-properties': {
+    spreadOptions: Array<{
+      object?: string
+      property?: string
+      message: string
+    }>
+  }
+  'no-restricted-syntax': {
+    spreadOptions: Array<{
+      selector: string
+      message: string
+    }>
+  }
+  'no-shadow': {
+    allow: Array<string>
+  }
+}
+
 declare const configurations: Array<Linter.FlatConfig>
 
 export default configurations
